17598f063008d480f258e8efa93ab4876524d53f
Ildar <NotfullinIF@kamaz.ru>
2025-08-01 15:26:23 +0300

    Вызов работает и не вылетает ошибка Segmentation fault, однако функция для создания STEP модели теперь находится вне класса MyWindow

--- main_window.py
+++ main_window.py
@@ -5,27 +5,30 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 
 from resources.main_window import Ui_main_window
 
+def run_cadquery():
+    """Функция, выполняемая в отдельном процессе"""
+    import cadquery as cq
+    from cadquery.vis import show
+
+    result = cq.Workplane("front").box(2, 3, 0.5)  # создаём простую призму
+    result = result.faces(">Z").workplane().hole(1)  # находим верхнюю грань и делаем отверстие
+
+    show(result)
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         self.ui = Ui_main_window()  # Создаем объект нашего интерфейса
         self.ui.setupUi(self)      # Устанавливаем наш интерфейс на главное окно
 
-        self.ui.pushButton_2.clicked.connect(self.create_and_show)
+        self.ui.pushButton_2.clicked.connect(self.on_button_click)
 
     
-    def create_and_show(self):
-        '''Функция, выполняемая в отдельном процессе'''
-
-        import cadquery as cq
-        from cadquery.vis import show
-
-        result = cq.Workplane("front").box(2, 3, 0.5)  # make a basic prism
-        result = (
-            result.faces(">Z").workplane().hole(1)
-        )  # find the top-most face and make a hole
-
-        show(result)
+    def on_button_click(self):
+        """ Обработчик события нажатия кнопки. Запускает создание и визуализацию 3D-модели в отдельном процессе. """
+        process = multiprocessing.Process(target=run_cadquery)
+        process.start()
+        process.join()  # дожидаемся окончания процесса (если хотите завершить синхронно)
 
 
 if __name__ == "__main__":
@@ -33,10 +36,3 @@ if __name__ == "__main__":
     window = MyWindow()              # Создаем экземпляр окна
     window.show()                    # Показываем окно
     app.exec()                       # Запускаем цикл обработки событий
-    
-    # p=multiprocessing.Process(target=create_and_show)
-    # p.start()
-
-    # p.join()
-
-    # sys.exit(0)
