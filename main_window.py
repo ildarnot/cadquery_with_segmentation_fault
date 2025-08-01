0c8c8952e68125f2773d14eaf03f76991ba85fbf
Ildar <NotfullinIF@kamaz.ru>
2025-08-01 15:23:25 +0300

    Версия с работающим вызовом построения модели в отдельном окне, без мультипроцессинга

--- main_window.py
+++ main_window.py
@@ -1,4 +1,5 @@
 import sys
+import multiprocessing
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
@@ -10,9 +11,32 @@ class MyWindow(QMainWindow):
         self.ui = Ui_main_window()  # Создаем объект нашего интерфейса
         self.ui.setupUi(self)      # Устанавливаем наш интерфейс на главное окно
 
+        self.ui.pushButton_2.clicked.connect(self.create_and_show)
+
+    
+    def create_and_show(self):
+        '''Функция, выполняемая в отдельном процессе'''
+
+        import cadquery as cq
+        from cadquery.vis import show
+
+        result = cq.Workplane("front").box(2, 3, 0.5)  # make a basic prism
+        result = (
+            result.faces(">Z").workplane().hole(1)
+        )  # find the top-most face and make a hole
+
+        show(result)
+
 
 if __name__ == "__main__":
     app = QApplication([])           # Инициализация приложения
     window = MyWindow()              # Создаем экземпляр окна
     window.show()                    # Показываем окно
     app.exec()                       # Запускаем цикл обработки событий
+    
+    # p=multiprocessing.Process(target=create_and_show)
+    # p.start()
+
+    # p.join()
+
+    # sys.exit(0)
