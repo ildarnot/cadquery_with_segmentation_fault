e266e92e58bdc412038b486fdc5edb7478d6d0d9
Ildar <NotfullinIF@kamaz.ru>
2025-08-26 10:41:27 +0300

    Добавлена многооконность на мультипроцесс с коробкой

--- multiproces_box_with_hole.py
+++ multiproces_box_with_hole.py
@@ -15,6 +15,29 @@ def run_cadquery(params):
     result = result.faces(">Z").workplane().hole(diameter)     # находим верхнюю грань и делаем отверстие
     show(result)
 
+class MyWindow_main(QMainWindow):
+    def __init__(self):
+        super(MyWindow_main, self).__init__()
+        self.ui = Ui_main_window()  # Создаем объект нашего интерфейса
+        self.ui.setupUi(self)      # Устанавливаем наш интерфейс на главное окно
+        
+        
+        # Создаем список для хранения открытых окон
+        self.open_windows = []
+        
+        # Классы окон модулей приложения, при добавлении нового окна нужно сюда добавлять классы       
+        self.classes = {
+            "MyWindow": MyWindow,
+        }
+        
+        self.ui.pushButton_2.clicked.connect(lambda: self.open_sliders_window(self.classes["MyWindow"])) # При нажатии на кнопку запускается окно построения шестерни
+              
+    def open_sliders_window(self, class_name, **kwargs):
+        self.sliders_window = class_name(**kwargs)
+        self.sliders_window.show()
+        # Сохраняем ссылку на новое окно в списке
+        self.open_windows.append(self.sliders_window)    
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
@@ -36,6 +59,6 @@ class MyWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)           # Инициализация приложения
-    window = MyWindow()              # Создаем экземпляр окна
+    window = MyWindow_main()              # Создаем экземпляр окна
     window.show()                    # Показываем окно
     sys.exit(app.exec())                     # Запускаем цикл обработки событий
